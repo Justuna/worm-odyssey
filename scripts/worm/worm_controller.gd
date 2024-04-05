4fde406fe858105ed0f65a8fe75d57e87543dbdb
Atlinx <atlinuxtm@gmail.com>
2024-04-04 20:06:29 -0400

    Add worm collision

--- scripts/worm/worm_controller.gd
+++ scripts/worm/worm_controller.gd
@@ -24,14 +24,14 @@ var tail_segment: WormSegment :
 var segments: Array[WormSegment]
 var head_position: Vector2 :
 	get:
-		return _head_position
+		return worm_head.global_position
 var head_direction: Vector2 :
 	get:
 		return _head_direction
 
 @export_category("Dependencies")
 @export var line_2D: Line2D
-@export var worm_head: Node2D
+@export var worm_head: CharacterBody2D
 @export var worm_tail: Node2D
 @export var worm_segment_prefab: PackedScene
 @export var camera: Camera2D
@@ -39,7 +39,6 @@ var head_direction: Vector2 :
 var _fixed_visual_segment_positions: Array[Vector2]
 var _visual_segment_positions: PackedVector2Array
 var _actual_direction: Vector2
-var _head_position: Vector2
 var _head_direction: Vector2
 var _prev_debug_draw: bool
 
@@ -50,7 +49,7 @@ func _ready():
 		segments.append(worm_segment_prefab.instantiate() as WormSegment)
 	_fixed_visual_segment_positions = []
 	var curr_segment_pos: Vector2 = global_position
-	_head_position = global_position
+	worm_head.global_position = global_position
 	for i in range(visual_segment_count):
 		_fixed_visual_segment_positions.push_front(curr_segment_pos)
 		curr_segment_pos.x -= visual_segment_length
@@ -66,13 +65,13 @@ func _process(delta):
 			_actual_direction = _head_direction.rotated(deg_to_rad(turning_cone_deg) * speed * delta * sign(angle_to_head)).normalized() * direction.length()
 		_head_direction = _actual_direction.normalized()
 		
-		var movement_delta = delta * speed
-		_head_position += movement_delta * _actual_direction
+		worm_head.velocity = _actual_direction * speed
+		worm_head.move_and_slide()
 		
 		# Next fixed visual point starts off at the current fixed visual head point
 		var next_fixed_visual_point = _fixed_visual_segment_positions[_fixed_visual_segment_positions.size() - 1]
-		var fixed_visual_head_to_real_head_dir = (_head_position - next_fixed_visual_point).normalized()
-		var fixed_visual_head_to_real_head_dist = next_fixed_visual_point.distance_to(_head_position)
+		var fixed_visual_head_to_real_head_dir = (worm_head.global_position - next_fixed_visual_point).normalized()
+		var fixed_visual_head_to_real_head_dist = next_fixed_visual_point.distance_to(worm_head.global_position)
 		while fixed_visual_head_to_real_head_dist >= visual_segment_length:
 			fixed_visual_head_to_real_head_dist -= visual_segment_length;
 			next_fixed_visual_point += visual_segment_length * fixed_visual_head_to_real_head_dir
@@ -87,11 +86,10 @@ func _process(delta):
 			_visual_segment_positions[i] = _fixed_visual_segment_positions[i].lerp(_fixed_visual_segment_positions[i + 1], lerp_amount_per_segment)
 	
 	# Make camera follow head, set line2D points, set head and tail sprites
-	camera.global_position = _head_position
+	camera.global_position = worm_head.global_position
 	line_2D.points = _visual_segment_positions
 	worm_tail.global_position = _visual_segment_positions[0]
 	worm_tail.rotation = (_visual_segment_positions[1] - _visual_segment_positions[0]).angle()
-	worm_head.global_position = _visual_segment_positions[_visual_segment_positions.size() - 1]
 	worm_head.rotation = (_visual_segment_positions[_visual_segment_positions.size() - 2] - _visual_segment_positions[_visual_segment_positions.size() - 1]).angle() + PI
 	
 	# Debug draw
@@ -104,12 +102,13 @@ func _process(delta):
 
 func _draw():
 	if debug_draw:
-		draw_line(_head_position, _head_position + _head_direction * 64, Color.RED, 3)
-		draw_line(_head_position, _head_position + direction * 64, Color.GREEN, 3)
+		draw_circle(worm_head.global_position + _actual_direction * 64, 8, Color.PURPLE)
+		draw_line(worm_head.global_position, worm_head.global_position + _head_direction * 64, Color.RED, 3)
+		draw_line(worm_head.global_position, worm_head.global_position + direction * 64, Color.GREEN, 3)
 		draw_polyline(PackedVector2Array(_fixed_visual_segment_positions), Color.RED)
 		for segment_pos in _fixed_visual_segment_positions:
 			draw_circle(segment_pos, 4, Color.RED)
 		draw_polyline(_visual_segment_positions, Color.BLUE)
 		for segment_pos in _visual_segment_positions:
 			draw_circle(segment_pos, 4, Color.BLUE)
-		draw_circle(_head_position, 4, Color.RED)
+		draw_circle(worm_head.global_position, 4, Color.RED)
