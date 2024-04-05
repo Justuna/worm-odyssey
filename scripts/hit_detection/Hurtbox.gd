64685c2f273a21e13ded9c0f3b2cf034c430f465
Justuna <jlpisa999@yahoo.com>
2024-04-04 20:24:32 -0400

    stubbing out hitbox stuff

---
+++ scripts/hit_detection/Hurtbox.gd
@@ -0,0 +1,7 @@
+# Defines an area representing an entity's vulnerability to damage.
+
+extends Area2D
+
+# Requirements:
+    # Be able to accept incoming damage/effects
+    # Maybe apply a damage multiplier for crit spots?
