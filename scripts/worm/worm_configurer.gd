038ea452a93038e2ebee9ccbc42a09a3abf33360
Atlinx <atlinuxtm@gmail.com>
2024-04-05 22:17:28 -0400

    Add config text

--- scripts/worm/worm_configurer.gd
+++ scripts/worm/worm_configurer.gd
@@ -3,8 +3,8 @@ class_name WormConfigurer
 
 
 @export var worm: Node2D
-@export var config_worm_controls_info: Control
-@export var worm_controls_info: Control
+@export var config_worm_ui: Control
+@export var worm_ui: Control
 @export var camera: CameraController
 @export var in_config_mode: bool :
 	get:
@@ -28,10 +28,10 @@ func toggle_config_mode():
 func _set_config_mode(value):
 	_in_config_mode = value
 	if is_inside_tree():
-		config_worm_controls_info.visible = _in_config_mode
-		worm_controls_info.visible = not _in_config_mode
+		config_worm_ui.visible = _in_config_mode
+		worm_ui.visible = not _in_config_mode
 		get_tree().paused = _in_config_mode
 		if _in_config_mode:
-			camera.set_target(worm_controller.worm_head)
-		else:
 			camera.set_target(worm_controller.segments[0])
+		else:
+			camera.set_target(worm_controller.worm_head)
