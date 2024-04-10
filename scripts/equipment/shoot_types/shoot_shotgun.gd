8a8d060e597ba9fcde7e5cda00943319de5f2484
Ray1212144 <147676185+Ray1212144@users.noreply.github.com>
2024-04-09 21:14:25 -0400

    Add random offset

--- scripts/equipment/shoot_types/shoot_shotgun.gd
+++ scripts/equipment/shoot_types/shoot_shotgun.gd
@@ -4,6 +4,7 @@ class_name ShootShotgun
 
 signal on_shoot
 
+@export var random_offset : float
 @export var fire_round : int = 1
 @export var fire_interval : float
 @export var bullet_number : int
@@ -21,7 +22,7 @@ func shoot():
 		for i in range(bullet_number) :
 			var bullet = bullet_prefab.instantiate() as Projectile
 			World.instance.add_child(bullet)
-			bullet.construct(muzzle.global_position, Node2DUtils.local_to_global_dir(muzzle, Vector2.from_angle(deg_to_rad(i*even_angle-angle/2-90))), team.team)
+			bullet.construct(muzzle.global_position, Node2DUtils.local_to_global_dir(muzzle, Vector2.from_angle(deg_to_rad(i*even_angle-angle/2-90 + randf_range(-random_offset,random_offset)))), team.team)
 		
 	on_shoot.emit()
 
